1d848947a9fb9330ba1dfa81a4c25e47d555f62a
Vaughan <39153417+vaughanthompson@users.noreply.github.com>
2026-08-04 15:16:45 +1200

    sync

---
+++ js/site.js
@@ -0,0 +1,290 @@
+﻿$(document).ready(function(){
+
+    // sitewide and activeLinking
+    /*
+    var str = window.location.pathname;
+    if (str.indexOf('/en/') >= 0) {
+        $('div.containMNav').load("inc-mobNavEN.html");
+        $('div#masthead').load("inc-topNavEN.html");
+        $('footer#siteWide').load("inc-footerEN.html");
+    } else if (str.indexOf('/de/') >= 0) {
+        $('div.containMNav').load("inc-mobNavDE.html");
+        $('div#masthead').load("inc-topNavDE.html");
+        $('footer#siteWide').load("inc-footerDE.html");
+    } else {
+        $('div.containMNav').load("/de/inc-mobNavDE.html");
+        $('div#masthead').load("/de/inc-topNavDE.html");
+        $('footer#siteWide').load("/de/inc-footerDE.html");
+    }
+    */
+  
+
+
+
+    $.localScroll({
+            target: 'body',
+            queue: false,
+            hash: false,
+            duration: 600,
+            easing: 'easeOutQuad',
+            offset: 0
+    });
+
+    // mobile top scroll
+    //$('#navMobile').hide();
+    
+    new Waypoint({
+    element: document.getElementsByTagName('body'),
+    handler: function(direction) {
+        if (direction === 'down') {
+            $('#navMobile').fadeTo(1000,'1.0');
+        }
+        else {
+            $('#navMobile').hide();
+        }
+    },
+        offset: '-50%'
+    });
+    new Waypoint({
+        element: document.getElementById('inpageNav'),
+        handler: function(direction) {
+            if (direction === 'down') {
+                $('header.sticky').animate({opacity: "show"}, {queue : false, duration:500});
+                $('#navHeader div#mblNavControl a .icon-ic-menu').addClass('darker');
+            }
+            else {
+                $('header.sticky').animate({opacity: "hide"}, {queue : false, duration:350});
+                $('#navHeader div#mblNavControl a .icon-ic-menu').removeClass('darker');
+            }
+        },
+            offset: '-10%'
+        })
+    new Waypoint({
+        element: document.getElementById('inpageNavEnd'),
+        handler: function(direction) {
+            if (direction === 'down') {
+                $('header.sticky').animate({opacity: "hide"}, {queue : false, duration:350});
+            }
+            else {
+                $('header.sticky').animate({opacity: "show"}, {queue : false, duration:350});
+            }
+        },
+            offset: '50%'
+        })
+
+
+       
+
+
+    $(".slickSlider").slick({
+        autoplay:true,
+        autoplaySpeed:5000,
+        arrows:false,
+        dots:true,
+        fade:true,
+        speed:2000,
+        pauseOnHover:false
+    });
+
+
+    
+
+
+    // mobile nav
+    $('#navClose').hide();
+    $('#navClose').click(function(){
+        $(this).hide();
+        $('#navCart').show();
+        $('#navTrigger').show();
+        $("div.mblnavContainer").scrollTo(0);
+        $('div.mblnav').hide();
+        $('html, body').removeClass('noscroll');
+        return false;
+    });
+
+    $('#navTrigger').click(function(){
+        $(this).hide();
+        $('#navCart').hide();
+        $('#navClose').show();
+        $('div.mblnav').fadeTo(500,'1.0');
+        $('html, body').addClass('noscroll');
+        return false;
+
+    });
+
+    $('.mblnav a.inpage').click(function(){
+        $('#navClose').hide();
+        $('#navCart').show();
+        $('#navTrigger').show();
+        $("div.mblnavContainer").scrollTo(0);
+        $('div.mblnav').hide();
+        $('html, body').removeClass('noscroll');
+        return false;
+    });
+
+
+    // slideNav
+    $('#omniDrop').click(function(){
+        //$(this).hide();
+        $(this).siblings('a').toggleClass('faded');
+        $(this).find('i').toggleClass('rotateIC');
+        $('#slideNav').slideToggle();
+        return false;
+
+    });
+
+
+
+
+    // show random div
+    var elems = $(".randomShow");
+    if (elems.length) {
+    var keep = Math.floor(Math.random() * elems.length);
+    for (var i = 0; i < elems.length; ++i) {
+        if (i !== keep) {
+        $(elems[i]).hide();
+        }
+    }
+    }
+
+
+
+    // named anchor scrollTo and offset
+    var anchorLink = $(window.location.hash);
+    if ( anchorLink.length ) {
+        $("html, body").animate({scrollTop: anchorLink.offset().top - 50 }, 500);
+    }
+
+
+
+
+    // onOff waypoints content area
+    $('.onOff').each(function() {
+        var inview = new Waypoint.Inview({
+        element: this,
+        enter: function(direction) {
+            // alert('Enter triggered with direction ' + direction)
+            },
+            entered: function(direction) {     
+                //$(this[0,'element']).find('.onOffTarget').removeClass('stateInactive');                            
+                $(this[0,'element']).find('.onOffTarget').addClass('fadeIn');
+                    
+            },
+            exit: function(direction) {
+                //$(this[0,'element']).find('.onOffTarget').removeClass('fadeIn');                            
+                //$(this[0,'element']).find('.onOffTarget').addClass('stateInactive');
+            },
+            exited: function(direction) {
+                // alert('Exited triggered with direction ' + direction)
+            }
+        })
+    });
+
+
+
+    //process page animations
+    $('.process1').delay(1000).fadeTo(2500,0.1);
+
+    $('.processTrigger1').each(function() {
+        var inview = new Waypoint.Inview({element: this,
+        enter: function(direction) {
+            $('.process2, .process3, .process4, .process5, .process6, .process7').stop(true, true).fadeTo(250,0);
+        }
+        })
+    });
+
+    $('.processTrigger2').each(function() {
+        var inview = new Waypoint.Inview({element: this,
+        entered: function(direction) {$('.process2').delay(0).fadeTo(750,1);}
+        })
+    });
+
+    $('.processTrigger3').each(function() {
+        var inview = new Waypoint.Inview({element: this,
+        entered: function(direction) {$('.process3').delay(0).fadeTo(750,1);}
+        })
+    });
+
+    $('.processTrigger4').each(function() {
+        var inview = new Waypoint.Inview({element: this,
+        entered: function(direction) {$('.process4').delay(0).fadeTo(750,1);}
+        })
+    });
+
+    $('.processTrigger5').each(function() {
+        var inview = new Waypoint.Inview({element: this,
+        entered: function(direction) {$('.process5').delay(0).fadeTo(750,1);}
+        })
+    });
+
+    $('.processTrigger6').each(function() {
+        var inview = new Waypoint.Inview({element: this,
+        entered: function(direction) {$('.process6').delay(0).fadeTo(750,1);}
+        })
+    });
+
+    $('.processTrigger7').each(function() {
+        var inview = new Waypoint.Inview({element: this,
+        entered: function(direction) {$('.process7').delay(0).fadeTo(750,1);}
+        })
+    });
+    
+    
+
+
+
+
+
+
+
+
+
+    $('.profileTrigger').click(function(){
+    //$(this).hide();
+    $('.profiles .activeDownArrow').addClass('inActive');
+    $('.profiles picture').addClass('greyscale');
+    $('.profileDetails > article').hide();
+    $(this).find('picture').removeClass('greyscale');
+    //$('.profiles picture').addClass('greyscale');
+    return false;
+    });
+
+
+    $('.profileTrigger1').click(function(){
+    $('.profiles .activeDownArrow1').removeClass('inActive');
+    $('.profileDetails > article.profile1').show();
+    return false;
+    });
+    
+
+    $('.profileTrigger2').click(function(){
+    $('.profiles .activeDownArrow2').removeClass('inActive');
+    $('.profileDetails > article.profile2').show();
+    return false;
+    });
+
+    $('.profileTrigger3').click(function(){
+    $('.profiles .activeDownArrow3').removeClass('inActive');
+    $('.profileDetails > article.profile3').show();
+    return false;
+    });
+
+    $('.profileTrigger4').click(function(){
+    $('.profiles .activeDownArrow4').removeClass('inActive');
+    $('.profileDetails > article.profile4').show();
+    return false;
+    });
+
+    $('.profileTrigger5').click(function(){
+    $('.profiles .activeDownArrow5').removeClass('inActive');
+    $('.profileDetails > article.profile5').show();
+    return false;
+    });
+
+
+
+
+
+
+});
+
